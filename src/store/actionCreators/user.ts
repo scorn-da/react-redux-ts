9cb285d7a7310e55bf96d0f35bc5bc99a0390aa6
Дмитрий Варганов <varganovda@gmail.com>
2023-07-26 16:20:22 +0300

    feat(code): added a user action creator

---
+++ src/store/actionCreators/user.ts
@@ -0,0 +1,18 @@
+import { UserActionTypes } from '../../types/user';
+import { Dispatch } from 'redux';
+import axios from 'axios';
+
+export const fetchUsers = () => {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch({ type: UserActionTypes.FETCH_USERS  });
+      const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+      dispatch( { type: UserActionTypes.FETCH_USERS_SUCCESS, payload: res.data });
+    } catch (e) {
+      dispatch({
+        type: UserActionTypes.FETCH_USERS_ERROR,
+        payload: 'Произошла ошибка при загрузке пользователей: ' + e.messageerror
+      });
+    }
+  }
+}
