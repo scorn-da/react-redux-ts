f1d2ae2695573c0e9421fbbb84f32f636a5b1d69
Дмитрий Варганов <varganovda@gmail.com>
2023-07-26 16:45:34 +0300

    feat(code, ui): fetched users and showed on the page

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from '../store/actionCreators/user';
 
 const UserList: React.FC = () => {
   const { users, error, isLoading } = useTypedSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const controller = new AbortController();
+      dispatch(fetchUsers());
+    return () => controller.abort();
+  }, [])
+
+  if (isLoading) {
+    return <h1>Загрузка...</h1>;
+  }
+
+  if (error) {
+    return <h1>{ error }</h1>;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <ul>
+      {users.map((user) => {
+        return (
+          <li key={user.id}>
+          {user.name}
+        </li>
+        )}
+      )}
+    </ul>
   );
 };
 
--- src/store/actionCreators/user.ts
+++ src/store/actionCreators/user.ts
@@ -2,16 +2,24 @@ import { UserActionTypes } from '../../types/user';
 import { Dispatch } from 'redux';
 import axios from 'axios';
 
-export const fetchUsers = () => {
+export const fetchUsers = (): any => {
   return async (dispatch: Dispatch) => {
     try {
       dispatch({ type: UserActionTypes.FETCH_USERS  });
       const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-      dispatch( { type: UserActionTypes.FETCH_USERS_SUCCESS, payload: res.data });
-    } catch (e) {
+      setTimeout(() => {
+        dispatch( { type: UserActionTypes.FETCH_USERS_SUCCESS, payload: res.data });
+      }, 500)
+    } catch (e: unknown) {
+      let message = 'Произошла ошибка при загрузке пользователей';
+
+      if (e instanceof Error) {
+        message += ': ' + e.message.toLowerCase();
+      }
+
       dispatch({
         type: UserActionTypes.FETCH_USERS_ERROR,
-        payload: 'Произошла ошибка при загрузке пользователей: ' + e.messageerror
+        payload: message
       });
     }
   }
