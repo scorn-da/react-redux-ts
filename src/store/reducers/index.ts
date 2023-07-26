8fe71fc49b570d387fdd498ed3e15e6480d404ad
Дмитрий Варганов <varganovda@gmail.com>
2023-07-26 22:19:34 +0300

    feat(code): added todoReducer in rootReducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { userReducer } from './userReducer';
+import { todosReducer } from './todosReducer';
 
 export const rootReducer = combineReducers({
   user: userReducer,
+  todo: todosReducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>;
