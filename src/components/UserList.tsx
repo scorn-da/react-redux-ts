e1a2b3f4d1b5514bb0d295b8f0fc4119c9207d52
Дмитрий Варганов <varganovda@gmail.com>
2023-07-26 16:05:16 +0300

    feat(store): added a root state type to use redux hooks with ts

---
+++ src/components/UserList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const UserList: React.FC = () => {
+  const state = useSelector(state => state);
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default UserList;
